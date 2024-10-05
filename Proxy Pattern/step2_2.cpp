473469cce74ec7b8d99222cac230a2dd0b91e1d3
huanxue <2759947937@qq.com>
2024-10-05 22:41:06 +0800

    Proxy/step2_2 Add_error handling_logic

--- Proxy Pattern/step2_2.cpp
+++ Proxy Pattern/step2_2.cpp
@@ -1,7 +1,10 @@
 
 #include <iostream>
+#include <regex>
 #include "my_print.cpp"
-
+#include  <string>
+using std::string;
+using std::regex;
 using namespace std;
 
 
@@ -21,13 +24,29 @@ class RealImage:public Image{
   string alternateText;
   string result;
   public:
+  /*
+    https?:\/\/：匹配 http:// 或 https://。
+    .*：匹配任意字符（除了换行符）零次或多次。
+    \.(jpg|jpeg|png|gif|bmp|svg|webp|icon)：匹配以图片格式结尾的部分。
+    $：表示字符串的结尾。
+  */
+  bool isImageUrl(const std::string& url) {
+    regex imageUrlRegex(R"(https?:\/\/.*\.(jpg|jpeg|png|gif|bmp|svg|webp|icon)$)",regex::icase);
+    return regex_match(url, imageUrlRegex);
+  }
   void display(string url){
     if(url==""){
     result="!["+alternateText+"]"+"("+"loading..."+")";
     print(result);
     }else{
-      result="!["+alternateText+"]"+"("+url+")";
-    print(result);
+      if(isImageUrl(url)){
+        result="!["+alternateText+"]"+"("+url+")";
+        print(result);
+      }
+      else{
+        result="!["+alternateText+"]"+"("+"nullpter"+")";
+        print(result);
+      }
     }
   }
 };
@@ -52,11 +71,13 @@ int main(){
   print("Initializing... ");
   myProxyImage->initImage();
   print("Displaying...");
-  myProxyImage->displayImage("https://learngitbranching.js.org/icon");
+  myProxyImage->displayImage("https://learngitbranching.js.org/xx.icon");
+  myProxyImage->displayImage("https://learngitbranching.js.org/i");
 }
 /* output:
   Initializing... 
   ![image of DesignPattern](loading...)
   Displaying...
-  ![image of DesignPattern](https://learngitbranching.js.org/icon)
+  ![image of DesignPattern](https://learngitbranching.js.org/xx.icon)
+  ![image of DesignPattern](nullpter)
 */ 
