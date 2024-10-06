7ec4537361eec8971912862a82c565e3259efae1
huanxue <2759947937@qq.com>
2024-10-06 14:22:13 +0800

    huanxue/COR/step2:Add default handler and enhance chain of responsibility
    - Implement a default handler for unclassified questions.
    - Extend the responsibility chain to include technical and customer service issues.
    - Integrate a system-wide problem-solving structure that categorizes and handles questions based on severity.
    - Introduce error handling for unmatched requests and recursion depth limit to prevent infinite loops.
    
    These changes aim to improve the modularity and robustness of the problem-handling system by ensuring that all requests are processed and prioritized appropriately.

--- Chain of Responsibility/step1.cpp
+++ Chain of Responsibility/step1.cpp
@@ -68,26 +68,44 @@ public:
         }
     }
 };
+class HandlerDefaultQuestion : public HandlerBase {
+public:
+    void handleRequest(string question) {
+        if (question == "") {
+            print("The question is null");
+            return;
+        } else {
+            printNonPln("The question is ",question," and the default solution was used.");
+            if (handler == nullptr) return;
+            handler->handleRequest(question);
+            return;
+        }
+    }
+};
 
 int main() {
     shared_ptr<HandlerBase> handlerQuestion = make_shared<HandlerBase>();
     shared_ptr<HandlerGeneralQuestion> handlerGeneralQuestion = make_shared<HandlerGeneralQuestion>();
     shared_ptr<HandlerImportantQuestion> handlerImportantQuestion = make_shared<HandlerImportantQuestion>();
     shared_ptr<HandlerUrgentQuestion> handlerUrgentQuestion = make_shared<HandlerUrgentQuestion>();
+    shared_ptr<HandlerDefaultQuestion> handlerDefaultQuestion = make_shared<HandlerDefaultQuestion>();
 
     handlerQuestion->setNext(handlerGeneralQuestion)
                    ->setNext(handlerImportantQuestion)
-                   ->setNext(handlerUrgentQuestion);
+                   ->setNext(handlerUrgentQuestion)
+                   ->setNext(handlerDefaultQuestion);
 
     handlerQuestion->handleRequest("");
     handlerQuestion->handleRequest("Urgent");
     handlerQuestion->handleRequest("General");
     handlerQuestion->handleRequest("Important");
+    handlerQuestion->handleRequest("Emerald Ore");
 
     return 0;
 }
 /*output:
-  Urgent question solution
-  General question solution
-  Important question solution
+    Urgent question solution
+    General question solution
+    Important question solution
+    The question is Emerald Ore and the default solution was used.
 */
